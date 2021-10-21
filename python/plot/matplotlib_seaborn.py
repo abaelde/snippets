cd2255a5faa8b5032ec2bf3a2df9f3065b90132e
abaelde <aurelien.baelde@gmail.com>
2021-10-21 15:16:27 +0200

    new commends)

--- python/plot/matplotlib_seaborn.py
+++ python/plot/matplotlib_seaborn.py
@@ -2,7 +2,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+# Normal matplotlib plots
+
+plt.xlabel("", fontsize=font_size)
+plt.ylabel("", fontsize=font_size)
+plt.xticks(rotation=rotation_angle)
+plt.grid()
+plt.show()
+
+
+
 # Subplot seaborn / create figure
+
 fig, axes = plt.subplots(1, 2, figsize=(15, 5))
 fig.suptitle("Figure Title")
 
@@ -10,7 +21,7 @@ sns.histplot(data=data, x="x axis data name", bins=50, ax=axes[0])
 #rotate labels / xticks
 plt.setp(axes[0].xaxis.get_majorticklabels(), rotation=45)
 # set axes labels
-axes[0].set_xlabel("xlabel)
+axes[0].set_xlabel("xlabel")
 axes[0].set_ylabel("tlabel")
 
 
