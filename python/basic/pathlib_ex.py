452efbafbbc6639672b97ff0d4ac318d7497961b
abaelde <aurelien.baelde@gmail.com>
2022-08-10 20:21:08 +0200

    Update pathlib_ex.py

--- python/basic/pathlib_ex.py
+++ python/basic/pathlib_ex.py
@@ -7,3 +7,7 @@ XX = pathlib.Path("PATH NAME")
 
 # get the folder path of the current file
 pathlib.Path(__file__).parent
+
+# Add suffix
+p = Path.home() / "image.png"               # "/Users/thibh/image.png"
+p.parent / (p.stem + "-lowres" + p.suffix)  # "/Users/thibh/image-lowres.png"
