a7c850875ff9f37a5b784b313570a06cbecf3a47
Aurélien Baelde <aurelien.baelde@gmail.com>
2022-09-17 12:11:13 +0200

    new

--- docker/k8s/k8s.sh
+++ docker/k8s/k8s.sh
@@ -1,3 +1,4 @@
+### CLUSTERS
 # Launch a minikube cluster with 1 node
 minikube start
 
@@ -6,3 +7,17 @@ kubectl get node
 
 # Suppression du cluster
 minikube delete
+
+
+### PODS
+# Lancer un pod
+kubectl apply -f [NOM_SPEC].yaml
+
+# Lister les pods
+kubectl get pods
+
+# Détails du pod
+kubectl describe pod [NOM_POD]
+
+# Suppression de pods
+kubectl delete po/[NOM_POD]
